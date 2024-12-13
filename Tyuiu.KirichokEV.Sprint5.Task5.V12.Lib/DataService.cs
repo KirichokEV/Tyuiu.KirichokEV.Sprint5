31f0f932bf42c28abef19902aeb11910a91c0d38
Elizaveta Kirichok <Tyuiu.KirichokEV@outlook.com>
2024-12-13 21:50:32 +0500

    Task5

---
+++ Tyuiu.KirichokEV.Sprint5.Task5.V12.Lib/DataService.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.KirichokEV.Sprint5.Task5.V12.Lib
+{
+    public class DataService : ISprint5Task5V12
+    {
+        public double LoadFromDataFile(string path)
+        {
+            
+            using (StreamReader reader = new StreamReader(path))
+            {
+                long positiveSum = 0;
+                long negativeSum = 0;
+
+                string[] lines = File.ReadAllLines(path);
+
+                foreach (string line in lines)
+                {
+                    if (long.TryParse(line, out long number))
+                    {
+                        if (number > 0)
+                        {
+                            positiveSum += number;
+                        }
+                        else if (number < 0)
+                        {
+                            negativeSum += number;
+                        }
+                    }
+                }
+                return positiveSum - negativeSum;
+            }
+        }
+    }
+}
---
+++ Tyuiu.KirichokEV.Sprint5.Task5.V12/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.KirichokEV.Sprint5.Task5.V12.Lib;
+
+namespace Tyuiu.KirichokEV.Sprint5.Task5.V12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнила: Киричoк Е. В.| РППб-24-1";
+
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                         *");
+            Console.WriteLine("* Тема: Класс File                                                                  *");
+            Console.WriteLine("* Задание #5                                                                        *");
+            Console.WriteLine("* Вариант #12                                                                       *");
+            Console.WriteLine("* Выполнила: Киричок Елиавета Викторовна | РППб-24-1                                *");
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                          *");
+            Console.WriteLine("*Создать папку в ручную С:\\DataSprint5\\ и скопировать в неё файл в котором есть   *");
+            Console.WriteLine("* набор значений.Найти разницу между суммой всех положительных и отрицательных целых*");
+            Console.WriteLine("* целых чисел в файле.Полученный результат вывести на консоль. У вещественных       *");
+            Console.WriteLine("*  значений округлить до трёх знаков после запятой.                                 *");
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                  *");
+            Console.WriteLine("*************************************************************************************");
+
+            string path = @"C:\Users\Sveta\AppData\Local\Temp\DataSprint5\InPutDataFileTask5V12.txt";
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                        *");
+            Console.WriteLine("*************************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
